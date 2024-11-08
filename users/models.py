15514ec21f0719fcb36b7f8b854bc75d60bebb09
Annie Rhodes <annierhodes17@gmail.com>
2024-11-08 11:07:47 +0000

    updated order form model

--- users/models.py
+++ users/models.py
@@ -31,16 +31,16 @@ class Address(models.Model):  # New Address Model
         return f'{self.full_name} - {self.street_address1}, {self.town_or_city}'
 
 class Order(models.Model):
-    order_number = models.CharField(max_length=32, null=False, editable=False, default='ORD0000')
+    order_number = models.CharField(max_length=32, null=False, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    full_name = models.CharField(max_length=50, null=False, blank=False, default='Guest')
-    email = models.EmailField(max_length=254, null=False, blank=False, default='unknown@example.com')
-    phone_number = models.CharField(max_length=20, null=False, blank=False, default='0000000000')
-    country = models.CharField(max_length=40, null=False, blank=False, default='Unknown')
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = models.CharField(max_length=40, null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
-    town_or_city = models.CharField(max_length=40, null=False, blank=False, default='Default Street Address')
-    street_address1 = models.CharField(max_length=80, null=False, blank=False, default='Default Street Address')
-    street_address2 = models.CharField(max_length=80, null=True, blank=True, default='Default Street Address')
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
@@ -74,8 +74,8 @@ class Order(models.Model):
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    color = models.CharField(max_length=30, null=True, blank=True)  # e.g., Red, Blue
-    gemstone = models.CharField(max_length=30, null=True, blank=True)  # e.g., Diamond, Ruby
+    color = models.CharField(max_length=30, null=True, blank=True)
+    gemstone = models.CharField(max_length=30, null=True, blank=True)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
